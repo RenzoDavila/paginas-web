2d65c4eeea3b13a5b0e13c120e10316be2f28bfb
RenzoDavila <rjacosta@stefanini.com>
2023-05-13 09:40:16 -0500

    cambios

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.sass']
 })
 export class AboutComponent {
+  experiencia:number = 0
 
+    ngOnInit(): void {
+      var currentTime = new Date();
+      var year = currentTime.getFullYear()
+      this.experiencia = Math.floor(year) - 1997
+      console.log(year);
+
+    }
 }
